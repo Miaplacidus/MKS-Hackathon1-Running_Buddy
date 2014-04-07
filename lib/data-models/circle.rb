9b7061b59690cd3320175562b1073676a95ea448
Wendy Hsu <wndyhsu@gmail.com>
2014-04-07 17:56:49 -0500

    update

--- lib/data-models/circle.rb
+++ lib/data-models/circle.rb
@@ -2,13 +2,15 @@ module RunB
   class Circle
     # NOTE JOIN TABLE: Lists all users belonging to a circle
     attr_reader :name
-    attr_accessor :id
+    attr_accessor :id, :creator_id, :joiner_id
 
   @@num_circles = 0
-    def initialize(name="Friends")
+    def initialize(name="Friends", creator_id, joiner_id)
       @@num_circles+=1
       @id = @@num_circles
       @name = name
+      @creator_id = creator_id
+      @joiner_id = joiner_id
     end
 
   end
