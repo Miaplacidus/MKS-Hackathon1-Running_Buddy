fb4d401698ae142b0eceac3126604dbd1b1e00d2
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-04-07 18:44:36 -0500

    Update MakeACommitment usecase and test

--- lib/client_helper.rb
+++ lib/client_helper.rb
@@ -8,7 +8,6 @@ require_relative 'data-models/post.rb'
 require_relative 'data-models/profile.rb'
 require_relative 'data-models/user.rb'
 require_relative 'data-models/database.rb'
-require_relative 'spec/usecases/signin.rb'
-require_relative 'spec/usecases/signup.rb'
 require_relative 'use_case.rb'
 Dir[File.dirname(__FILE__) + '/usecases/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + 'spec/usecases/*.rb'].each {|file| require file }
--- spec/usecases/post_a_run_spec.rb
+++ spec/usecases/post_a_run_spec.rb
@@ -7,32 +7,32 @@ describe 'PostARun' do
     @postTest = @db.create_post(@userTest.id, "11:00AM CST", "12th Street, Austin, TX", 4, 10)
   end
 
-  it "errors, if creator_id is missing" do
+  it "errors, if creator_id is wrong" do
     result = RunB::PostARun.run(:creator_id => 20)
     expect(result.error).to eq(:wrong_creator_id)
     expect(result.error?).to eq(true)
   end
-  xit "errors, if time is missing" do
+  xit "errors, if time is wrong" do
     result = RunB::PostARun.run(:creator_id => @userTest.id, :time => "12:00PM CST")
     expect(result.error).to eq(:wrong_time)
     expect(result.error?).to eq(true)
   end
-  xit "errors, if location is missing" do
+  xit "errors, if location is wrong" do
     result = RunB::PostARun.run(:creator_id => @userTest.id, :time => "11:00AM CST", :location => "wrong address")
     expect(result.error).to eq(:wrong_location)
     expect(result.error?).to eq(true)
   end
-  xit "errors, if pace is missing" do
+  xit "errors, if pace is wrong" do
     result = RunB::PostARun.run(:creator_id => @userTest.id, :time => "12:00PM CST", :location => "12th Street, Austin, TX", :pace => 2)
     expect(result.error).to eq(:wrong_pace)
     expect(result.error?).to eq(true)
   end
-  xit "errors, if minimum commitment is missing" do
+  xit "errors, if minimum commitment is wrong" do
     result = RunB::PostARun.run(:creator_id => @userTest.id, :time => "12:00PM CST", :location => "12th Street, Austin, TX", :pace => 4, :min_commit => 15)
     expect(result.error).to eq(:wrong_min_commit)
     expect(result.error?).to eq(true)
   end
-  xit "passes, " do
+  xit "passes, if everything is there" do
     result = RunB::PostARun.run(:creator_id => @userTest.id, :time => "12:00PM CST", :location => "12th Street, Austin, TX", :pace => 4, :min_commit => 10)
     expect(result.success?).to eq(true)
   end
