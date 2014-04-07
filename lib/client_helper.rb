7a0fc41df3aef87d4f5507baed1340e87c50065d
Wendy Hsu <wndyhsu@gmail.com>
2014-04-07 10:32:27 -0500

    updated

--- lib/client_helper.rb
+++ lib/client_helper.rb
@@ -7,5 +7,8 @@ require_relative 'data-models/commitment.rb'
 require_relative 'data-models/post.rb'
 require_relative 'data-models/profile.rb'
 require_relative 'data-models/user.rb'
+require_relative 'data-models/database.rb'
+require_relative 'usecases/signin.rb'
+require_relative 'usecases/signup.rb'
 require_relative 'use_case.rb'
 Dir[File.dirname(__FILE__) + '/use_cases/*.rb'].each {|file| require file }
