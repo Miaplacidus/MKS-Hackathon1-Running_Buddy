7eb0826796847f2a9fbea2e08ecb6117e4550926
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-04-07 18:54:11 -0500

    Just kidding, forgot to update make_a_commitment usecase

--- lib/usecases/make_a_commitment.rb
+++ lib/usecases/make_a_commitment.rb
@@ -2,7 +2,12 @@ module RunB
 
   class MakeACommitment < UseCase
     def run(inputs)
+      commitment = RunB.db.create_commitment(inputs[:user_id], inputs[:amount], inputs[:post_id])
+      return failure(:wrong_user_id) if inputs[:user_id] == nil
+      return failure(:amount_not_acceptable) if inputs[:amount] > 15
+      return failure(:wrong_post_id) if inputs[:post_id] == nil
 
+      return success(:commitment => commitment)
     end
 
   end
