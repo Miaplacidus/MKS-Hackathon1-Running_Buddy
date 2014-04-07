df3af782a323c60f1bc355bdbeeab6e3a660dcea
Wendy Hsu <wndyhsu@gmail.com>
2014-04-07 14:11:50 -0500

    completed

--- reset_db.rb
+++ reset_db.rb
@@ -15,20 +15,55 @@ sqlite.execute %q{DROP TABLE IF EXISTS commitments}
 puts "Creating tables..."
 sqlite.execute %q{
  CREATE TABLE users (
-   id          INTEGER PRIMARY KEY AUTOINCREMENT,
+   id          INTEGER  PRIMARY KEY,
    name        TEXT     NOT NULL,
    password    INT      NOT NULL,
    age         INT      NOT NULL,
    email       TEXT     NOT NULL,
-   level       INT
+   level       INT      NOT NULL,
+   rating      INT      NOT NULL,
+   budp_id     INT      NOT NULL,
  );
 }
 sqlite.execute %q{
- CREATE TABLE friendships (
-   id                                  INTEGER                         PRIMARY KEY,
-   user_source_id    INT           NOT NULL,
-   user_target_id                INT                        NOT NULL,
-   FOREIGN KEY(user_source_id) REFERENCES users(id),
-   FOREIGN KEY(user_target_id) REFERENCES users(id)
+ CREATE TABLE sessions(
+   id          INTEGER   PRIMARY KEY,
+   user_id     INT       NOT NULL,
+   FOREIGN KEY(user_id) REFERENCES users(id),
  );
 }
+sqlite.execute %q{
+ CREATE TABLE  circles(
+   id          INTEGER   PRIMARY KEY,
+   id          INT       NOT NULL,
+   name        TEXT      NOT NULL,
+ );
+}
+sqlite.execute %q{
+ CREATE TABLE posts(
+   id          INTEGER   PRIMARY KEY,
+   id          INT       NOT NULL,
+   creator_id  INT       NOT NULL,
+   time        INT       NOT NULL,
+   location    TEXT      NOT NULL,
+   pace        INT       NOT NULL,
+   complete    TEXT      NOT NULL,
+   min_amt     INT       NOT NULL,
+   FOREIGN KEY(creator_id) REFERENCES users(id),
+ );
+}
+sqlite.execute %q{
+ CREATE TABLE  commitments(
+   id          INTEGER   PRIMARY KEY,
+   user_id     INT       NOT NULL,
+   fulfilled   TEXT      NOT NULL,
+   amount      INT       NOT NULL,
+   id          INT       NOT NULL,
+   FOREIGN KEY(user_id) REFERENCES users(id)
+ );
+}
+
+
+puts "Database Schema:\n\n"
+puts `echo .schema | sqlite3 #{db_name}`
+
