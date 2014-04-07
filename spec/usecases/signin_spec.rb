71a4af44a1927d40e812d7299088ba2a2c8c95e5
Wendy Hsu <wndyhsu@gmail.com>
2014-04-07 10:35:58 -0500

    wrote specs for signin usecase

--- spec/usecases/signin_spec.rb
+++ spec/usecases/signin_spec.rb
@@ -1,1 +1,27 @@
+require 'spec_helper'
+
+describe 'SignIn' do
+  before do
+    @db = RunB.db
+  end
+
+  it "errors, if username doesn't exist " do
+    result = RunB::SignIn.run(:username => "Tom", :pw => "1234")
+    expect(result.error).to eq(:username_missing)
+    expect(result.error?).to eq(true)
+  end
+  xit "errors, if password is incorret" do
+    wendy = @db.add_user("Wendy", "23")
+    result = RunB::SignIn.run(:username =>"Wendy", :pw => "134")
+    expect(result.error).to eq(:incorrect_password)
+    expect(result.error?).to eq(true)
+  end
+  xit "passes, if both username and password matches" do
+    wendy = @db.add_user("Wendy", "1234")
+    result = RunB::SignIn.run(:username => "Wendy", :pw=>"1234")
+    expect(result.success?).to eq(true)
+    expect(result.session_id).to be_a(Integer)
+    expect(@db.get_user_from_session(result.session_id).name).to eq("Wendy")
+  end
+end
 
