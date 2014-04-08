45240f024da54544dd4e6fb34c65c8ef43b2b967
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-04-08 13:40:13 -0500

    db spec udpates

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -26,20 +26,20 @@ describe 'Database' do
   end
 
   describe 'Posts/Runs' do
-    xit 'can create a post and retrieve it' do
+    it 'can create a post and retrieve it' do
       post1 = @db.create_post(user1.id, "8am", "716 Congress Ave., Austin, TX")
       result = @db.get_post(postTest.id)
       expect(result).to eq(post1)
     end
 
-    xit 'can list all posts/runs' do
+    it 'can list all posts/runs' do
       post2 = @db.create_post(user1.id, "8am", "716 Congress Ave., Austin, TX")
       post3 = @db.create_post(user2.id, "6pm", "1300 Crossing Place, Austin, TX")
       post4 = @db.create_post(user3.id, "8pm", "Downtown, Austin, TX")
       # expect(@db.ls_post).to eq({post2, post3, post4})
     end
 
-    xit "can delete a post/run" do
+    it "can delete a post/run" do
       post5 = @db.create_post(user1.id, "8am", "716 Congress Ave., Austin, TX")
       post6 = @db.create_post(user2.id, "6pm", "1300 Crossing Place, Austin, TX")
       post7 = @db.create_post(user3.id, "8pm", "Downtown, Austin, TX")
