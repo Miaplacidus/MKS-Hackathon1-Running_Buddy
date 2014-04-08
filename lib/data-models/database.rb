9637a38bbd5c645f9eee00001abd8f4e550b080a
Ifu Aniemeka <ianiemeka@gmail.com>
2014-04-08 15:40:38 -0500

    Changes to database

--- lib/data-models/database.rb
+++ lib/data-models/database.rb
@@ -95,7 +95,8 @@ module RunB
       new_bpref = self.create_buddy_pref(data_hash)
       @sqlite.execute("INSERT INTO posts (creator_id, time, location, pace, min_amt, complete, budp_id) VALUES (?,?,?,?,?,?,?);", data_hash[:creator_id], data_hash[:time], data_hash[:location], data_hash[:pace], data_hash[:min_amt], data_hash[:complete], new_bpref.id)
       new_post.id = @sqlite.execute("SELECT last_insert_rowid()")[0][0]
-      self.update_post(new_post.id, {:post_id => new_post.id})
+      data_hash[:post_id] = new_post.id
+      self.update_post(new_post.id, data_hash)
       new_post
     end
 
@@ -131,10 +132,11 @@ module RunB
         if data_hash[:min_amt]
           @sqlite.execute("UPDATE posts SET min_amt = ? WHERE id = ?", data_hash[:min_amt], post_id)
         end
-        if (data_hash[:buddy_age] || data_hash[:buddy_gender] || data_hash[:post_id])
+        if (data_hash[:buddy_age] || data_hash[:buddy_gender])
+          data_hash[:post_id] = post_id
           rows = @sqlite.execute("SELECT * FROM posts WHERE id = ?", post_id)
           data = rows.first
-          self.update_buddy_pref(data[3], data_hash)
+          self.update_buddy_pref(data[7], data_hash)
         end
       self.get_post(post_id)
     end
@@ -301,7 +303,7 @@ module RunB
         rows = @sqlite.execute("SELECT * FROM buddyprefs WHERE id = ?", bf_id)
         data = rows.first
         if !data[3]
-          @sqlite.execute("INSERT INTO buddyprefs (post_id);", data_hash[:post_id])
+          @sqlite.execute("UPDATE buddyprefs SET post_id = ? WHERE id = ?;", data_hash[:post_id], bf_id)
         end
       end
     end
